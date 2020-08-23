524e152393afde32f3812405cc369013b69d211b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-23 15:16:50 +1000

    working demo

--- simpleMount.ts
+++ simpleMount.ts
@@ -1,4 +1,4 @@
-import { h, createApp } from 'vue'
+import { h, createApp, ComponentPublicInstance } from 'vue'
 
 export function simpleMount(originalComponent: any) {
   document.getElementsByTagName('html')[0].innerHTML = ''
@@ -9,19 +9,19 @@ export function simpleMount(originalComponent: any) {
   const el = document.createElement('div')
   document.body.appendChild(el)
 
-  const Parent = {
-    name: 'Parent',
+  const MountParent = {
+    name: 'MountParent',
     render() {
       return h(wrapped, { ref: 'REF' })
     }
   }
 
   // create the app
-  const app = createApp(Parent)
+  const app = createApp(MountParent)
 
   // mount the app!
   const vm = app.mount(el)
 
   // get ref to component
-  return vm.$refs['REF']
+  return vm.$refs['REF'] as ComponentPublicInstance
 }
